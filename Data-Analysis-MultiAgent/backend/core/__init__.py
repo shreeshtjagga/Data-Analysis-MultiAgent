6cfaf218006c8985c71196bb23e355f99545086b
iam-nagabalaji <nagabalajikothuri@gmail.com>
2026-04-04 12:20:35 +0530

    fixed app.py and made some changes in agents

--- Data-Analysis-MultiAgent/backend/core/__init__.py
+++ Data-Analysis-MultiAgent/backend/core/__init__.py
@@ -1,4 +1,4 @@
-from backend.core.state import AnalysisState
-from backend.core.graph import run_pipeline
+from core.state import AnalysisState
+from core.graph import run_pipeline
 
 __all__ = ["AnalysisState", "run_pipeline"]
